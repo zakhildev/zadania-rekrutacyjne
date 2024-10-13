92ba4bfca295abef03c26e51965f131123686013
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-10-13 14:49:51 +0200

    java init

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Z podanego zbioru danych wyselekcjonuj 5 o największej wartości na jednostkę, znając kategorię obiektu</br>
+ * Dane znajdują się w folderze "dane" w pliku "zbiór_wejściowy.json" oraz "kategorie.json"
+ * Wynik przedstaw w czytelnej formie na standardowym wyjściu
+ *
+ *
+ */
+
+public class Main {
+}
